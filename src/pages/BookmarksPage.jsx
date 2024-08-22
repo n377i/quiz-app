dabfa37e87693a12fb9591e2bbb9ce262f6bcc8e
Annette Wöhler <annettewoehler@gmx.de>
2024-08-22 21:52:50 +0200

    + Add BookmarksPage

---
+++ src/pages/BookmarksPage.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import Bookmarks from "../components/Bookmarks";
+
+const BookmarksPage = () => {
+  return (
+    <Layout isSubPage title="Bookmarks">
+      <Bookmarks />
+    </Layout>
+  );
+};
+
+export default BookmarksPage;
