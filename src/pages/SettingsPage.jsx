112f9964f7ee0d9d1553411e4a0258d4c96b8b47
Annette Wöhler <annettewoehler@gmx.de>
2024-08-22 21:51:46 +0200

    + Add SettingsPage

---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import Settings from "../components/Settings";
+
+const SettingsPage = () => {
+  return (
+    <Layout isSubPage title="Settings">
+      <Settings />
+    </Layout>
+  );
+};
+
+export default SettingsPage;
