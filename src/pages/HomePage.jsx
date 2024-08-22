44471a1fbb6956a456720cfe7f63f18746b5d3fb
Annette Wöhler <annettewoehler@gmx.de>
2024-08-22 21:52:14 +0200

    + Add HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import Home from "../components/Home";
+
+const HomePage = () => {
+  return (
+    <Layout title="Quiz App">
+      <Home />
+    </Layout>
+  );
+};
+
+export default HomePage;
