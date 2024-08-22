9ea2a5bd203299cbb164d984986633fdee6f78a5
Annette Wöhler <annettewoehler@gmx.de>
2024-08-22 20:07:59 +0200

    + Add routes

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BookmarksPage from "./pages/BookmarksPage";
+import FormPage from "./pages/FormPage";
+import SettingsPage from "./pages/SettingsPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/bookmarks" element={<BookmarksPage />} />
+        <Route path="/form" element={<FormPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
