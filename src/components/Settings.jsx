bd6ce76eaafedfa3dbbf53af904202b0a345f53a
Annette Wöhler <annettewoehler@gmx.de>
2024-08-24 00:25:45 +0200

    + Show number of quiz cards and bookmarked cards

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "../components/Card.css";
 import "../components/Settings.css";
+import QuizCardIcon from "../assets/icons/icon_quiz-card.svg";
+import BookmarkIcon from "../assets/icons/icon_bookmark_selected.svg";
 
 const Settings = ({ darkMode, setDarkMode }) => {
+  const [totalQuizCards, setTotalQuizCards] = useState(0);
+  const [totalBookmarkedCards, setTotalBookmarkedCards] = useState(0);
+
+  useEffect(() => {
+    const storedCards = JSON.parse(localStorage.getItem("quizCards")) || [];
+    setTotalQuizCards(storedCards.length);
+    const bookmarkedCount = storedCards.filter(
+      (card) => card.isBookmarked
+    ).length;
+    setTotalBookmarkedCards(bookmarkedCount);
+  }, []);
+
   const handleToggleTheme = () => {
     setDarkMode((prevMode) => !prevMode);
   };
@@ -11,27 +26,20 @@ const Settings = ({ darkMode, setDarkMode }) => {
       <h2 className="card__title">Stats</h2>
       <div className="card__stats">
         <div className="card__counter">
-          <span
-            className="card__counter-icon card__counter-icon--questions"
-            role="img"
-            aria-label="created questions"
-          ></span>
-          <span className="card__counter-number" data-js="num-of-created-cards">
-            0
-          </span>
+          <img
+            src={QuizCardIcon}
+            alt="Number of created quiz cards"
+            className="card__counter-icon"
+          />
+          <span className="card__counter-number">{totalQuizCards}</span>
         </div>
         <div className="card__counter">
-          <span
-            className="card__counter-icon card__counter-icon--bookmark"
-            role="img"
-            aria-label="created bookmarks"
-          ></span>
-          <span
-            className="card__counter-number"
-            data-js="num-of-bookmarked-cards"
-          >
-            0
-          </span>
+          <img
+            src={BookmarkIcon}
+            alt="Number of bookmarked cards"
+            className="card__counter-icon"
+          />
+          <span className="card__counter-number">{totalBookmarkedCards}</span>
         </div>
       </div>
       <div className="card__settings">
