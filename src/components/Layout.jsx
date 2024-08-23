e1cee3088b1c5372c982a65ba501363b625935a8
Annette Wöhler <annettewoehler@gmx.de>
2024-08-24 00:31:46 +0200

    + Add isEditMode prop

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,18 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Navigation from "../components/Navigation";
 
-const Layout = ({ title, isSubPage, children, isActive }) => {
+const Layout = ({
+  title,
+  isSubPage,
+  children,
+  isActive,
+  isEditMode = false,
+}) => {
   return (
     <>
       <Header isSubPage={isSubPage} title={title} />
       <Main>{children}</Main>
-      <Navigation isActive={isActive} />
+      <Navigation isActive={isActive} isEditMode={isEditMode} />
     </>
   );
 };
